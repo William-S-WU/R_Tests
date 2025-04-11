6b18f8092b6b0207765ab34738c353bbc903ecd5
William Schmitt <schmittw@wustl.edu>
2025-04-11 17:07:46 -0500

    Update still WIP

--- RPractice.R
+++ RPractice.R
@@ -4,6 +4,7 @@ library(corrplot)
 getwd()
 print(getwd())
 current_directory <- getwd()
+
 df <- read.csv("wine-quality.csv")
 head(df)
 column_names <- colnames(df)
@@ -19,12 +20,18 @@ plot(df$citric.acid, df$quality, main="Scatter Plot")
 hist(df$citric.acid)
 cor(df$citric.acid, df$volatile.acidity)  
 
+#simple tests
+t.test(df$quality ~ df$citric.acid)
+wilcox.test(df$quality ~ df$citric.acid)
 
+#model linear
 model <- lm(quality ~ citric.acid, data=df)
 summary(model)  # Check results
 multi_model <- lm(quality ~ . , data=df)
 summary(multi_model)
 
+
+#PCA 
 typeof(column_names)
 pca_result <- prcomp(df, scale=TRUE)
 summary(pca_result)
@@ -33,13 +40,30 @@ pca_result$x
 
 biplot(pca_result)
 
+explained_variance <- pca_result$sdev^2 / sum(pca_result$sdev^2) * 100
+print(explained_variance)
+
+#PCA visualization
+x_label <- paste0("Principal Component 1 (", round(explained_variance[1], 2), ")%")
+y_label <- paste0("Principal Component 2 (", round(explained_variance[2], 2), ")%")
+
+pca_data <- data.frame(pca_result$x)
+pca_data$quality <- df$quality
+ggplot(pca_data, aes(x = PC1, y = PC2, color = as.factor(quality))) +
+  geom_point() +
+  labs(title = 'PCA of Wine Quality Data', x = x_label, y = y_label, color = 'Quality') +
+  theme_minimal()
+
+
+
+#eigen values
 eigenvalues <- pca_result$sdev^2
 print(eigenvalues)
 
 loadings <- pca_result$rotation
 print(loadings)
 
-data.frame(Eigenvalues=eigenvalues, Loadings=t(loadings))
+dataEigenvalues_loading <- data.frame(Eigenvalues=eigenvalues, Loadings=t(loadings))
 
 
-corrplot(loadings, method="circle")
+corrplot(loadings, method="circle") #correlation plot of loadings
